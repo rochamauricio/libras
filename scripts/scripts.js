94e2beac7de245483eaed099805f2b3e1da76104
mauricio <mauricio.mbr@gmail.com>
2023-03-14 00:07:46 -0300

    Adiciona legenda para sinais até a letra b

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,57 +5,47 @@ var titulo = document.getElementById('titulo');
 var opcao = document.getElementById('opcoes');
 var legendaTeste = document.getElementById('legendaTeste');
 
-arrayPos = 14;
+var listaFiltrada = [];
+var posicaoLista = 0;
+
+filtrar();
+
+function filtrar() {
+    listaFiltrada = dicionario.filter(item => item.tipo == opcao.value);
+    posicaoLista = 0;
+    exibirSinal(0);
+
+    if (opcao.value == "numero")
+        titulo.innerHTML = "Números";
+    else if (opcao.value == "alfabeto")
+        titulo.innerHTML = "Letras do Alfabeto";
+    else if (opcao.value == "a")
+        titulo.innerHTML = "Palávras com A";       
+    else if (opcao.value == "b")
+        titulo.innerHTML = "Palávras com B";    
+}
+
+function exibirSinal(posicaoLista) {
+    if (listaFiltrada.length > 0) {
+        imgTeste.src = listaFiltrada[posicaoLista].url;
+        legendaTeste.innerHTML = listaFiltrada[posicaoLista].palavra;
+    }
+}
 
 function proxima() {
-    arrayPos += 1;
-    if (opcao.value == "numero" && arrayPos >= 14)
-        arrayPos = 0;
-    else if (opcao.value == "alfabeto" && arrayPos > 40)
-        arrayPos = 14;
-    else if (opcao.value == "palavrasA" && arrayPos >= 119) {
-        arrayPos = 41;
-    }
-
-    imgTeste.src = dicionario[arrayPos].url;
-    legendaTeste.innerHTML = dicionario[arrayPos].palavra;
+    posicaoLista += 1;
+    if (posicaoLista >= listaFiltrada.length)
+        posicaoLista = 0;
+    exibirSinal(posicaoLista);
 }
 
 function anterior() {
-
-    arrayPos -= 1;
-    if (opcao.value == "numero" && arrayPos < 0)
-        arrayPos = 13;
-    else if (opcao.value == "alfabeto" && arrayPos < 14)
-        arrayPos = 40;
-    else if (opcao.value == "palavrasA" && arrayPos < 41) {
-        arrayPos = 118;
-    }
-
-    imgTeste.src = dicionario[arrayPos].url;
-    legendaTeste.innerHTML = dicionario[arrayPos].palavra;
+    posicaoLista -= 1;
+    if (posicaoLista <= 0)
+        posicaoLista = listaFiltrada.length - 1;
+    exibirSinal(posicaoLista);
 }  
-
-function mudaOpcao() {
-    if (opcao.value == "numero") {
-        titulo.innerHTML = "Números";
-        arrayPos = 0;    
-        imgTeste.src = dicionario[arrayPos].url;
-        legendaTeste.innerHTML = dicionario[arrayPos].palavra;
-    }
-    else if (opcao.value == "alfabeto") {
-        titulo.innerHTML = "Letras do Alfabeto";
-        arrayPos = 14;
-        imgTeste.src = dicionario[arrayPos].url;
-        legendaTeste.innerHTML = dicionario[arrayPos].palavra;
-    } else if (opcao.value == "palavrasA"){
-        titulo.innerHTML = "Palávras com A";
-        arrayPos = 41;
-        imgTeste.src = dicionario[arrayPos].url;
-        legendaTeste.innerHTML = dicionario[arrayPos].palavra;
-    }
-}
 
 botaoProximo.addEventListener('click', proxima);
 botaoAnterior.addEventListener('click', anterior);
-opcoes.addEventListener('change', mudaOpcao);
+opcao.addEventListener('change', filtrar);
